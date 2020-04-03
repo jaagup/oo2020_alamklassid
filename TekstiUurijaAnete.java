f2c0049f0d8742a9188f81a0ffbfbaedf4befee0
Anete Vaalu <anetevlu@tlu.ee>
2020-04-03 13:36:47 +0300

    täheloendur

--- TekstiUurijaAnete.java
+++ TekstiUurijaAnete.java
@@ -2,4 +2,15 @@ public class TekstiUurijaAnete implements TekstiUurija{
 	public int t2htedeArv(String s){
 		return s.length();
 	}
+
+	int loendur;
+	public int loenda(String s, char c){
+		for(int i = 0; i< s.length(); i++){
+			if(s.charAt(i) == c){
+				loendur++;
+			}
+		}
+
+		return loendur;
+	}
 }
