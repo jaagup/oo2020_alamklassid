5f0e0bd1ed7e43671925272d85609884010fb915
cadaroba <kadarob@greeny.cs.tlu.ee>
2020-04-03 13:48:14 +0300

    uuendatud

--- TekstiTestid.java
+++ TekstiTestid.java
@@ -6,4 +6,9 @@ public class TekstiTestid{
 	public void pikkus(){
 		assertEquals(7, t.t2htedeArv("telefon"));
 	}
+	@Test
+	public void loendamine1(){
+		assertEquals(2, t.loenda("tere", 'e'));
+		assertEquals(3, t.loenda("terekest", 'e'));
+	}
 }
