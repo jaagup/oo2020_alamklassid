852e0cd84589a1b62a9a55e1489f2a9a817db187
Jaagup Kippar <jaagup@tlu.ee>
2020-04-03 13:39:02 +0300

    loendamine

--- TekstiUurijaJaagup.java
+++ TekstiUurijaJaagup.java
@@ -2,4 +2,11 @@ public class TekstiUurijaJaagup implements TekstiUurija{
 	public int t2htedeArv(String s){
 		return s.length();
 	}
+	public int loenda(String s, char c){
+		int loendur=0;
+		for(char abi: s.toCharArray()){
+			if(abi==c){loendur++;}
+		}
+		return loendur;
+	}
 }
