8377e7c13a49781dc8269d3a492dbd40991dbd6a
Jaagup Kippar <jaagup@tlu.ee>
2020-04-04 22:20:25 +0300

    taastatud failid

--- TekstiTestidJaagup.java
+++ TekstiTestidJaagup.java
@@ -3,3 +3,11 @@ public class TekstiTestidJaagup extends TekstiTestid{
         t=new TekstiUurijaJaagup();
     }
 }
+/*
+[jaagup@greeny oo2020_alamklassid]$ java -cp junit4.jar:. org.junit.runner.JUnitCore TekstiTestidJaagup
+JUnit version 4.8.2
+...
+Time: 0.036
+
+OK (3 tests)
+*/
